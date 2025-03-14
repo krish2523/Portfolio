b55d606a84f7830736966a2a47e3ac8ae54f1fa6
krish2523 <147299723+krish2523@users.noreply.github.com>
2025-03-14 20:55:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:["FrontEnd Developer", "Web Developer"],
+    strings:["Full Stack Developer", "Web Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
